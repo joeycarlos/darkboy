3293ac10dde76fbc05c44985149e32c7f0b17593
joeycarlos <jclaguio@gmail.com>
2019-11-26 17:29:16 -0800

    Add player move and camera follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private static CameraController _instance;
+
+    public static CameraController Instance {
+        get {
+            if (_instance == null) {
+                GameObject go = new GameObject("CameraController");
+                go.AddComponent<CameraController>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public GameObject player;
+    public float horizontalOffset = 0f;
+    public float verticalOffset = 0f;
+
+    void Awake() {
+        _instance = this;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        if (player != null)
+            transform.position = new Vector3(player.transform.position.x + horizontalOffset, verticalOffset, -10.0f);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    private BoxCollider2D bc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessMovementInput();
+    }
+
+    void ProcessMovementInput() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (horizontalInput != 0) {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Mathf.Sign(horizontalInput) * Vector2.right, (bc.bounds.size.x / 2.0f + 0.5f), LayerMask.GetMask("Wall"));
+            if (hit.collider == null) {
+                Move(horizontalInput, moveSpeed);
+            }
+        }
+    }
+
+    void Move(float horizontalInput, float moveSpeed) {
+        Vector3 moveVector = new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0);
+        transform.Translate(moveVector);
+    }
+}

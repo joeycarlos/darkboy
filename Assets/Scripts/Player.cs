8b6cec5ff2cdb1ac53ee71a02d283b45c0eb22c9
joeycarlos <jclaguio@gmail.com>
2019-11-30 11:57:52 -0800

    Clean up player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     // MOVEMENT
     [Header("Moving")]
     [SerializeField] private float moveSpeed = 5.0f;
     private bool isFacingRight;
+    private float horizontalInput;
 
     // JUMPING
     [Header("Jumping")]
@@ -21,6 +21,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float isGroundedRememberTime = 0.15f;
     private float isGroundedRememberCounter;
     private int platformLayer;
+    private bool isGrounded;
 
     // ATTACKING
     [Header("Attacking")]
@@ -28,16 +29,18 @@ public class Player : MonoBehaviour
     [SerializeField] private float attackRange = 0.5f;
     [SerializeField] private float damage = 3.0f;
     [SerializeField] private float knockbackPower = 5.0f;
+    private bool attackFinished;
 
     // HEALTH
     [Header("Health")]
     public int maxHealth = 5;
     private int currentHealth;
 
-    // SELF KNOCKBACK
+    // HURT STATE
     [Header("Hurt State")]
     [SerializeField] private float hurtTime = 0.3f;
     [SerializeField] private float selfKnockbackPower = 3.0f;
+    private float hurtTimeCounter;
 
     // SPIRIT
     [Header("Spirit")]
@@ -53,35 +56,16 @@ public class Player : MonoBehaviour
     // STATE
     [HideInInspector] public State state;
 
-    // INPUTS
-    float horizontalInput;
-    bool isGrounded;
-    float hurtTimeCounter;
-    bool attackFinished;
-
-    // -----
-
     public enum State { Idle, Running, Jumping, Falling, Charging, Attacking, Hurt, Death, Pause };
 
     void Start() {
-        bc = GetComponent<BoxCollider2D>();
-        rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
-
-        state = State.Idle;
-        isFacingRight = true;
-        jumpTimeCounter = 0;
-        currentHealth = maxHealth;
-        isGroundedRememberCounter = 0;
-        hasJumped = false;
-        currentSpirit = 0;
-        spiritLevel = 1;
-
+        LinkComponents();
+        InitPlayerVars();
         InitPlayerUI();
     }
 
     void Update() {
-        ReadInputs();
+        horizontalInput = Input.GetAxis("Horizontal");
         isGrounded = CheckGround();
 
         switch (state) {
@@ -118,7 +102,7 @@ public class Player : MonoBehaviour
                 if (Input.GetKeyUp(KeyCode.UpArrow) || jumpTimeCounter <= 0) {
                     state = State.Falling;
                 }
-                    
+
                 break;
             case State.Falling:
                 ProcessMovementInput();
@@ -169,32 +153,52 @@ public class Player : MonoBehaviour
                 break;
             default:
                 break;
-
-           
         }
     }
 
-    void ReadInputs() {
-        horizontalInput = Input.GetAxis("Horizontal");
+    // INIT HELPER FUNCTIONS
+
+    void LinkComponents() {
+        bc = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    void ProcessMovementInput() {
+    void InitPlayerUI() {
+        GameplayUI.Instance.GenerateHealthUI(maxHealth);
+        GameplayUI.Instance.UpdateSpiritLevelValue(spiritLevel);
+        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().min = 0;
+        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().max = spiritLevelReqs[spiritLevel - 1];
+        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().SetSpirit(currentSpirit);
+    }
 
-        // Face the correct direction
+    void InitPlayerVars() {
+        state = State.Idle;
+        isFacingRight = true;
+        jumpTimeCounter = 0;
+        currentHealth = maxHealth;
+        isGroundedRememberCounter = 0;
+        hasJumped = false;
+        currentSpirit = 0;
+        spiritLevel = 1;
+    }
+
+    // MOVEMENT HELPER FUNCTIONS
+
+    void ProcessMovementInput() {
         if ((isFacingRight == true && horizontalInput < 0) || (isFacingRight == false && horizontalInput > 0)) {
             attackPos.localPosition = new Vector3(-attackPos.localPosition.x, attackPos.localPosition.y, attackPos.localPosition.z);
             isFacingRight = !isFacingRight;
         }
 
-        // Move if no wall and and not in knockback state
         if (horizontalInput != 0) {
-
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Mathf.Sign(horizontalInput) * Vector2.right, (bc.bounds.size.x / 2.0f + 0.5f), LayerMask.GetMask("Wall"));
-
             if (hit.collider == null)
                 transform.Translate(new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0));
         }
     }
+
+    // JUMPING AND GROUNDED HELPER FUNCTIONS
 
     void EnterJumpState() {
         jumpTimeCounter = maxJumpTime;
@@ -202,7 +206,6 @@ public class Player : MonoBehaviour
     }
 
     bool CheckGround() {
-
         bool result1, result2, result3;
 
         Vector3 raycastOriginOffset = new Vector3(-bc.bounds.extents.x, -bc.bounds.extents.y + 0.05f, 0);
@@ -228,19 +231,17 @@ public class Player : MonoBehaviour
         }
         else
             return false;
-        
     }
 
+    // HURT STATE HELPER FUNCTIONS
+
     public bool isHittable() {
-        if (state == State.Hurt)
-            return false;
-        else
-            return true;
+        if (state == State.Hurt) return false;
+        else return true;
     }
 
     public void EnterHurtState(int damage, bool isRightDirection) {
         state = State.Hurt;
-
         TakeDamage(damage);
         Knockback(isRightDirection);
         sr.color = Color.red;
@@ -270,8 +271,7 @@ public class Player : MonoBehaviour
         Vector2 knockbackVector;
         if (isRightDirection) {
             knockbackVector = new Vector2(knockbackPower, knockbackPower + 2.0f);
-        }
-        else {
+        } else {
             knockbackVector = new Vector2(-knockbackPower, knockbackPower + 2.0f);
         }
         rb.AddForce(knockbackVector, ForceMode2D.Impulse);
@@ -281,198 +281,7 @@ public class Player : MonoBehaviour
         Destroy(gameObject);
     }
 
-    void InitPlayerUI() {
-        GameplayUI.Instance.GenerateHealthUI(maxHealth);
-
-        GameplayUI.Instance.UpdateSpiritLevelValue(spiritLevel);
-        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().min = 0;
-        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().max = spiritLevelReqs[spiritLevel - 1];
-        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().SetSpirit(currentSpirit);
-        
-    }
-
-    void AddSpirit(int value) {
-        if (currentSpirit + value >= spiritLevelReqs[spiritLevel - 1]) {
-            currentSpirit = currentSpirit + value - spiritLevelReqs[spiritLevel - 1];
-            LevelUp();
-        }
-        else {
-            currentSpirit += value;
-        }
-        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().SetSpirit(currentSpirit);
-    }
-
-    void LevelUp() {
-        spiritLevel++;
-        GameplayUI.Instance.UpdateSpiritLevelValue(spiritLevel);
-    }
-
-    private void OnTriggerEnter2D(Collider2D col) {
-        if (col.gameObject.layer == LayerMask.NameToLayer("Pickup")) {
-            Pickup p = col.gameObject.GetComponent<Pickup>();
-            AddSpirit(p.spiritValue);
-            Destroy(p.gameObject);
-        }
-    }
-
-    /*
-    // Start is called before the first frame update
-    void Start()
-    {
-        bc = GetComponent<BoxCollider2D>();
-        rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
-        
-        platformLayer = LayerMask.GetMask("Platform");
-        isJumping = false;
-        isGroundedRemember = 0;
-        isFacingRight = true;
-        currentHealth = maxHealth;
-        knockbackState = false;
-        currentSpirit = 0;
-        spiritLevel = 1;
-
-        InitPlayerUI();
-    }
-
-    void Update() {
-        ProcessMovementInput();
-        GroundedCheck();
-        ProcessJumpInput();
-        ProcessAttackInput();
-    }
-
-    void ProcessMovementInput() {
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        // Face the correct direction
-        if ((isFacingRight == true && horizontalInput < 0) || (isFacingRight == false && horizontalInput > 0)) {
-            attackPos.localPosition = new Vector3(-attackPos.localPosition.x, attackPos.localPosition.y, attackPos.localPosition.z);
-            isFacingRight = !isFacingRight;
-        }
-
-        // Move if no wall and and not in knockback state
-        if (horizontalInput != 0 && knockbackState == false) {
-
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Mathf.Sign(horizontalInput) * Vector2.right, (bc.bounds.size.x / 2.0f + 0.5f), LayerMask.GetMask("Wall"));
-
-            if (hit.collider == null)
-                transform.Translate(new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0));
-        }
-    }
-
-    void GroundedCheck() {
-        if (isGroundedRemember > 0)
-            isGroundedRemember -= Time.deltaTime;
-
-        bool result1, result2, result3;
-
-        Vector3 raycastOriginOffset = new Vector3(-(bc.size.x), -bc.size.y/2.0f + 0.05f, 0);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastOriginOffset, -Vector2.up, 1.0f, platformLayer);
-        if (hit.collider == null) result1 = false;
-        else result1 = true;
-
-        raycastOriginOffset = new Vector3(bc.size.x, -bc.size.y / 2.0f + 0.05f, 0);
-        hit = Physics2D.Raycast(transform.position + raycastOriginOffset, -Vector2.up, 1.0f, platformLayer);
-        if (hit.collider == null) result2 = false;
-        else result2 = true;
-
-        raycastOriginOffset = new Vector3(0, -bc.size.y / 2.0f, 0);
-        hit = Physics2D.Raycast(transform.position + raycastOriginOffset, -Vector2.up, 1.0f, platformLayer);
-        if (hit.collider == null) result3 = false;
-        else result3 = true;
-
-        if (result1 == true || result2 == true || result3 == true) {
-            isGroundedRemember = isGroundedRememberTime;
-        }
-    }
-
-    void ProcessJumpInput() {
-
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGroundedRemember > 0) {
-            isJumping = true;
-            jumpTimeCounter = maxJumpTime;
-            rb.velocity = Vector2.up * jumpForce;
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow) && isJumping == true) {
-            if (jumpTimeCounter > 0) {
-                rb.velocity = Vector2.up * jumpForce;
-                jumpTimeCounter -= Time.deltaTime;
-            }
-            else
-                isJumping = false;
-        }
-
-        if (Input.GetKeyUp(KeyCode.UpArrow)) {
-            isJumping = false;
-        }
-    }
-
-    void ProcessAttackInput() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, LayerMask.GetMask("Enemy"));
-            for (int i = 0; i < enemiesToDamage.Length; i++) {
-                if (enemiesToDamage[i].GetComponent<Enemy>().knockbackState == false) {
-                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
-                    enemiesToDamage[i].GetComponent<Enemy>().Knockback(knockbackPower, isFacingRight);
-                }
-            }
-        }
-    }
-
-    public void TakeDamage(int damage) {
-        int originalHealth = currentHealth;
-
-        currentHealth = currentHealth - damage;
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-
-        GameplayUI.Instance.RemoveHealthIcon(-(currentHealth - originalHealth));
-
-        if (currentHealth <= 0) {
-            Death();
-        }
-    }
-
-    public void Knockback(bool isRightDirection) {
-        rb.velocity = Vector2.zero;
-
-        Vector2 knockbackVector;
-        if (isRightDirection) {
-            knockbackVector = new Vector2(knockbackPower, knockbackPower + 2.0f);
-            Debug.Log(knockbackVector);
-        }
-        else {
-            knockbackVector = new Vector2(-knockbackPower, knockbackPower + 2.0f);
-        }
-        rb.AddForce(knockbackVector, ForceMode2D.Impulse);
-        StartCoroutine(KnockbackState(knockbackTime));
-    }
-
-    public void AddHealth(int value) {
-        int originalHealth = currentHealth;
-        currentHealth += value;
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-        GameplayUI.Instance.AddHealthIcon(currentHealth - originalHealth);
-    }
-
-    void Death() {
-        Destroy(gameObject);
-    }
-
-    private void OnDrawGizmosSelected() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, attackRange);
-    }
-
-    IEnumerator KnockbackState(float time) {
-        knockbackState = true;
-        sr.color = Color.red;
-        yield return new WaitForSeconds(time);
-
-        knockbackState = false;
-        sr.color = Color.white;
-    }
+    // SPIRIT AND HEALTH HELPER FUNCTIONS
 
     void AddSpirit(int value) {
         if (currentSpirit + value >= spiritLevelReqs[spiritLevel - 1]) {
@@ -489,14 +298,14 @@ public class Player : MonoBehaviour
         GameplayUI.Instance.UpdateSpiritLevelValue(spiritLevel);
     }
 
-    void InitPlayerUI() {
-        GameplayUI.Instance.GenerateHealthUI(maxHealth);
-        GameplayUI.Instance.UpdateSpiritLevelValue(spiritLevel);
-
-        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().min = 0;
-        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().max = spiritLevelReqs[spiritLevel - 1];
-        GameplayUI.Instance.GetComponentInChildren<SpiritBar>().SetSpirit(currentSpirit);
+    public void AddHealth(int value) {
+        int originalHealth = currentHealth;
+        currentHealth += value;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        GameplayUI.Instance.AddHealthIcon(currentHealth - originalHealth);
     }
+
+    // TRIGGERS AND COLLISIONS
 
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.layer == LayerMask.NameToLayer("Pickup")) {
@@ -505,7 +314,26 @@ public class Player : MonoBehaviour
             Destroy(p.gameObject);
         }
     }
-    */
+}
+    /*
+
+    void ProcessAttackInput() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, LayerMask.GetMask("Enemy"));
+            for (int i = 0; i < enemiesToDamage.Length; i++) {
+                if (enemiesToDamage[i].GetComponent<Enemy>().knockbackState == false) {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                    enemiesToDamage[i].GetComponent<Enemy>().Knockback(knockbackPower, isFacingRight);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
 
 
 }
+*/
